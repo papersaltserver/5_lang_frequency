004add3da5ae5ce3d2f24f775b04b07a59c47350
Dmitry Trukhanov <noconnect@gmail.com>
2017-11-16 13:46:46 +0300

    First version

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+This script allows you to find out top-10 most frequent file in any text.
+
+# Usage
+ 
+ To use this script you need Python 3.5 installed.
+ To run this script on linux:
+ ```bash
+ python lang_frequency.py path_to_your_text
+ ```
+ Sample output:
+```
+der
+die
+und
+das
+in
+er
+ich
+ist
+auch
+sie
+```
+ 
+ Some distributions will need to use python3 command instead of python. 
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,23 @@
+from collections import Counter
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as text_file:
+        text_data = text_file.read()
+    return text_data
 
 
 def get_most_frequent_words(text):
-    pass
+    text_list = text.strip("\\\",.\'\n").lower().split()
+    text_counter = Counter(text_list)
+    most_common_list_counter = text_counter.most_common(10)
+    most_common_list = [list_tuple[0] for list_tuple in most_common_list_counter]
+    return most_common_list
 
 
 if __name__ == '__main__':
-    pass
+    text_data = load_data(sys.argv[1])
+    most_frequent_words = get_most_frequent_words(text_data)
+    for frequent_word in most_frequent_words:
+        print(frequent_word)
