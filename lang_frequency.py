ab076dd96649fe32f65dd0221c53ccebb651a45d
Dmitry Trukhanov <noconnect@gmail.com>
2017-11-16 14:19:43 +0300

    Changed 10 frequent words to any number of frequent words with variable

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,16 +8,17 @@ def load_data(filepath):
     return text_data
 
 
-def get_most_frequent_words(text):
+def get_most_frequent_words(text, frequent_words_number):
     text_list = text.strip("\\\",.\'\n").lower().split()
     text_counter = Counter(text_list)
-    most_common_list_counter = text_counter.most_common(10)
+    most_common_list_counter = text_counter.most_common(frequent_words_number)
     most_common_list = [list_tuple[0] for list_tuple in most_common_list_counter]
     return most_common_list
 
 
 if __name__ == '__main__':
     text_data = load_data(sys.argv[1])
-    most_frequent_words = get_most_frequent_words(text_data)
+    frequent_words_number = 10
+    most_frequent_words = get_most_frequent_words(text_data, frequent_words_number)
     for frequent_word in most_frequent_words:
         print(frequent_word)
--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class LangFrequencyTests(unittest.TestCase):
         "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore "
         "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " 
         "mollit anim id est laborum.")
-        test_result_list = get_most_frequent_words(test_text)
+        test_result_list = get_most_frequent_words(test_text, 1)
         self.assertTrue(test_result_list[0] == "ut")
 
 
