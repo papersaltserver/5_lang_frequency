4fc1be1d5750f866112aeeaa43a30df973c3db47
papersaltserver <noconnect@gmail.com>
2017-11-17 21:14:26 +0300

    Shortened lines with autopep8 suggestions

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,5 +1,6 @@
 from collections import Counter
 import sys
+import re
 
 
 def load_data(filepath):
@@ -9,9 +10,10 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text, frequent_words_number):
-    word_list = text.strip("\\\",.\'\n").lower().split()
+    word_list = re.sub(r'\W+_', '', text).lower().split()
     words_in_text_counter = Counter(word_list)
-    most_common_list_counter = words_in_text_counter.most_common(frequent_words_number)
+    most_common_list_counter = words_in_text_counter.most_common(
+        frequent_words_number)
     most_common_list = [word for (word, _) in most_common_list_counter]
     return most_common_list
 
@@ -19,7 +21,9 @@ def get_most_frequent_words(text, frequent_words_number):
 if __name__ == '__main__':
     text_data = load_data(sys.argv[1])
     frequent_words_number = 10
-    most_frequent_words = get_most_frequent_words(text_data, frequent_words_number)
-    print("The list of {} most frequent words in your text in descending order: ".format(frequent_words_number))
+    most_frequent_words = get_most_frequent_words(
+        text_data, frequent_words_number)
+    print("The list of {} most frequent words in your text in \
+    descending order: ".format(frequent_words_number))
     for frequent_word in most_frequent_words:
         print(frequent_word)
